cc3f86ccdb6e137ac7a7a5c58b50e91eda815c0e
Chreees79 <christelle.gevaert32@gmail.com>
2022-12-12 17:46:01 +0100

    add individual forms for edit informations and shipping

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ import {RegisterPage} from "@pages/authentification/registerPage";
 import ResetPasswordPage from "@pages/authentification/resetPasswordPage";
 import NewPasswordPage from "@pages/authentification/newPasswordPage";
 import NewUser from "@pages/profile/profileFormPage";
-import EditUser from "@pages/profile/editProfileFormPage";
+import EditInformations from "@pages/profile/editProfileInformationsPage";
+import EditShipping from "@pages/profile/editProfileShippingPage";
 import ProfileOverview from "@pages/profile/detailsProfilePage";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -76,7 +77,8 @@ export const App = observer(() => {
             <Route path="/new_project" element={<PrivateRoute component={<NewProject />}/>}/>
             <Route path="/new_supplier" element={<PrivateRoute component={<NewSupplier />}/>}/>
             <Route path="/profile" element={<ProfileOverview />} />
-            <Route path="/edit_profile" element={<PrivateRoute component={<EditUser />}/>}/>
+            <Route path="/edit_informations" element={<PrivateRoute component={<EditInformations />}/>}/>
+            <Route path="/edit_shipping" element={<PrivateRoute component={<EditShipping />}/>}/>
             <Route path="/404" element={<Error404/>}/>
             <Route path="/confirmation" element={<EmailValidation/>}/>
           </Routes>
